4497350613efbdd48c96c6b6eba0917e9c67694e
TSL-24 <Justint.matter@gmail.com>
2024-12-06 13:39:53 -0500

    Created Employee Models

--- Seed_To_Sale/Employees/models.py
+++ Seed_To_Sale/Employees/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+
+	emp_id = models.IntegerField(primary_key=True)
+	emp_is_active = models.BooleanField()
+	emp_is_manager = models.BooleanField(default=False)
+	emp_first_name = models.CharField(max_length=50)
+	emp_last_name = models.CharField(max_length=50)
+	emp_dob = models.DateField()
+	emp_start_date = models.DateField()
+	
+	def __str__(self):
+		return f"Employee ID:{self.emp_id}, Employee Name: {self.emp_first_name} {self.emp_last_name}"
