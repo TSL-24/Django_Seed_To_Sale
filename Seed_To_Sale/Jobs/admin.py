d691a4132e8719be38ce4782c4d89f8c90803c92
TSL-24 <Justint.matter@gmail.com>
2024-12-06 16:23:04 -0500

    Updated Job Models
    - added job_desc to Job model
    - created TaskType model
    - created Task model
    - updated model field names

--- Seed_To_Sale/Jobs/admin.py
+++ Seed_To_Sale/Jobs/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Job, JobType, TaskLog
+from .models import Job, JobType, TaskLog, TaskType, Task
 
 admin.site.register(Job)
 admin.site.register(JobType)
 admin.site.register(TaskLog)
+admin.site.register(TaskType)
+admin.site.register(Task)
--- Seed_To_Sale/Jobs/models.py
+++ Seed_To_Sale/Jobs/models.py
@@ -2,16 +2,17 @@ from django.db import models
 from Employees.models import Employee 
 
 class JobType(models.Model):
-	type_id = models.IntegerField(primary_key=True)
-	type_name = models.CharField(max_length=100)
+	job_type_id = models.IntegerField(primary_key=True)
+	job_type_name = models.CharField(max_length=100)
 
 	def __str__(self):
-		return(self.type_name)
+		return(self.job_type_name)
 
 class Job(models.Model):
 	job_id = models.AutoField(primary_key=True)
 	job_is_complete = models.BooleanField()
 	job_name = models.CharField(max_length=200)
+	job_desc = models.CharField(max_length=500)
 	job_type = models.ForeignKey(JobType, on_delete=models.CASCADE)
 	job_create_date = models.DateTimeField(auto_now_add=True)
 	job_start = models.DateTimeField(auto_now_add=False)
@@ -22,9 +23,31 @@ class Job(models.Model):
 	def __str__ (self):
 		return(self.job_name)
 
-class TaskLog(models.Model):
+class TaskType(models.Model):
+	task_type_id = models.IntegerField(primary_key=True)
+	task_type_parent = models.ForeignKey(JobType, on_delete=models.CASCADE)
+	task_type_name = models.CharField(max_length=100)
+	task_type_desc = models.CharField(max_length=500, blank=True)
+
+	def __str__(self):
+		return(self.task_name)
+
+class Task(models.Model):
 	task_id = models.AutoField(primary_key=True)
+	task_is_complete = models.BooleanField()
+	task_name = models.CharField(max_length=100)
+	task_desc = models.CharField(max_length=500)
+	task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
 	task_job = models.ForeignKey(Job, on_delete=models.CASCADE)
-	employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
-	task_tstamp = models.DateTimeField(auto_now_add=True)
-	task_data = models.JSONField()
+
+	def __str__(self):
+		return(self.task_name)
+
+class TaskLog(models.Model):
+	task_log_id = models.AutoField(primary_key=True)
+	task_log_job = models.ForeignKey(Job, on_delete=models.CASCADE)
+	task_log_employee_id = models.ForeignKey(Employee, on_delete=models.CASCADE)
+	task_log_tstamp = models.DateTimeField(auto_now_add=True)
+	task_log_data = models.JSONField()
+
+	
